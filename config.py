cf6439df0392224122200745d01ed83366b5be1a
Sweet Dev <goodcristina2023@gmail.com>
2024-08-01 07:00:35 -0700

    first commit

---
+++ config.py
@@ -0,0 +1,33 @@
+from enum import Enum
+from glob import glob
+from logging import getLogger
+from logging.config import fileConfig
+from os.path import join, isfile
+from os import environ
+from dotenv import load_dotenv
+
+PROJECT_ROOT = ""
+LOG_INI = join(PROJECT_ROOT, 'log.ini')
+FIXTURES = join(PROJECT_ROOT, 'fixtures')
+
+def load_env():
+    load_dotenv(join(PROJECT_ROOT, ".env"))
+
+class PromptTemplate(Enum):
+    SYSTEM_PROMPT = "system.txt"
+    
+    
+
+def get_prompt_template(prompt_template: PromptTemplate):
+    with open(join(PROJECT_ROOT, "prompt_templates", prompt_template.value), "rt") as f:
+        return f.read()
+
+
+def configure_logging(get_logger=False):
+    global logging_configured
+    if not logging_configured:
+        fileConfig(LOG_INI)
+        logging_configured = True
+    if get_logger:
+        logger = getLogger('freedo')
+        return logger
